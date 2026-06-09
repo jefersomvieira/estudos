eaad5a621076e6ee9454f7e8a753493d72d5d066
Jeferson Vieira de Lima <120428083+jefersomvieira@users.noreply.github.com>
2026-06-08 23:45:03 -0300

    aulas de javascript
    algumas revisoes

---
+++ Curso-em-video/JavaScript/aulas de reforço/JavaScript/condições/script2.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {//condição composta
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
